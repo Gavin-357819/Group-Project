7e969cc6d586f89516c46a28514b0963b63a59c6
Gavin-357819 <Gavin.357819@nv.ccsd.net>
2023-05-05 10:26:41 -0700

    main menu and endscreen added

---
+++ Group_projectV3/Assets/Menu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Next()
+    {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+}
